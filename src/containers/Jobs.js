3ced0dde43e6685437263a094b726e38338b15ca
LIManuel <leonmalisov@gmail.com>
2020-10-06 12:57:25 -0400

    imported jobs data from local file

--- src/containers/Jobs.js
+++ src/containers/Jobs.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import JobListing from "../components/JobListing"
+
+let jobData = require('../project_files/data.json')
 
 class Jobs extends Component {
 
 	render() {
 		return (
 			<div id="reviews-overview" className="overview-wrapper">
-				HOWDY
+				
 			</div>
 		)
 	}
