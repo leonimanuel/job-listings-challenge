4c9d4a33cff1f9b60f0fbe46ef81876b6f90c1b7
LIManuel <leonmalisov@gmail.com>
2020-10-06 13:01:55 -0400

    createJobListing renders JobListing for every job in jobData

--- src/containers/Jobs.js
+++ src/containers/Jobs.js
@@ -4,11 +4,14 @@ import JobListing from "../components/JobListing"
 let jobData = require('../project_files/data.json')
 
 class Jobs extends Component {
+	createJobListings = () => {
+		return jobData.map(job => <JobListing job={job}/>)
+	}
 
 	render() {
 		return (
 			<div id="reviews-overview" className="overview-wrapper">
-				
+				{this.createJobListings()}
 			</div>
 		)
 	}
