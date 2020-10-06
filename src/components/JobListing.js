b264cde5aebf0807516cd927d40e25336f8a4570
LIManuel <leonmalisov@gmail.com>
2020-10-06 13:50:23 -0400

    job listing styling

--- src/components/JobListing.js
+++ src/components/JobListing.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import "./job-listing.scss"
 
+    // "new": true,
+    // "featured": true,
+    // "position": "Senior Frontend Developer",
+    // "role": "Frontend",
+    // "level": "Senior",
+    // "postedAt": "1d ago",
+    // "contract": "Full Time",
+    // "location": "USA Only",
+    // "languages": ["HTML", "CSS", "JavaScript"],
+    // "tools": []
+
 class JobListing extends Component {
 	render() {
+		const { job } = this.props
 		return (
-			<div id="reviews-overview" className="overview-wrapper">
-				<div>{this.props.job.company}</div>
+			<div className="job-wrapper">
+				<div>{job.company}</div>
 			</div>
 		)
 	}
 }
 
 export default JobListing;
+
+
+
+
+
