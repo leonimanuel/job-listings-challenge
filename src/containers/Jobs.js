6db04c059f72d987d1c25f60ac1c06387e44cfc6
LIManuel <leonmalisov@gmail.com>
2020-10-06 12:50:32 -0400

    created Jobs container

---
+++ src/containers/Jobs.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Jobs extends Component {
+
+	render() {
+		return (
+			<div id="reviews-overview" className="overview-wrapper">
+				HOWDY
+			</div>
+		)
+	}
+}
+
+export default Jobs;
