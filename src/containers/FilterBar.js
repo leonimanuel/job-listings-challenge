4203b5920fc9067703cd7862bcdb9627b33b71b3
LIManuel <leonmalisov@gmail.com>
2020-10-10 13:11:56 -0400

    enclosed filterbar contents in new div

--- src/containers/FilterBar.js
+++ src/containers/FilterBar.js
@@ -7,11 +7,13 @@ class FilterBar extends Component {
 	render() {
 		return (
 			<div id="filter-bar">
-				<div id="filters-container">
-					{this.props.filters.map(filter => <Filter filter={filter} onClickRemove={() => this.props.onFilterRemove(filter)}/>)}			
-				</div>	
-				
-				<div id="clear-filters-button" onClick={this.props.onClearFilters}>Clear</div>		
+				<div id="filter-bar-content">
+					<div id="filters-container">
+						{this.props.filters.map(filter => <Filter filter={filter} onClickRemove={() => this.props.onFilterRemove(filter)}/>)}			
+					</div>	
+					
+					<div id="clear-filters-button" onClick={this.props.onClearFilters}>Clear</div>		
+				</div>
 			</div>
 		)
 	}
