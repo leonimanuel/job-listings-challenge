120aff619cb1abd251d33713ed64fc877a28e659
LIManuel <leonmalisov@gmail.com>
2020-10-09 20:45:47 -0400

    conditionally render 'new' and 'featured' tags

--- src/components/JobListing.js
+++ src/components/JobListing.js
@@ -18,7 +18,11 @@ class JobListing extends Component {
 		return (
 			<div className="job-wrapper">
 				<div className="details-wrapper">
-					<div className="company">{job.company}</div>
+					<div className="company-and-highlights-wrapper">
+						<div className="company">{job.company}</div>
+						{job.new ? <div className="new-tag">NEW!</div> : null}
+						{job.featured ? <div className="featured-tag">FEATURED</div> : null}
+					</div>
 					<div className="position">{job.position}</div>
 					<div className="posting-details">
 						<div className="postedAt">{job.postedAt}</div>
@@ -27,10 +31,10 @@ class JobListing extends Component {
 					</div>
 				</div>
 				<div className="tags-wrapper">
-						<div className="tag role">{job.role}</div>
-						<div className="tag level">{job.level}</div>
-						{job.languages.map(lang => <div className="tag language">{lang}</div>)}
-						{job.tools.map(tool => <div className="tag language">{tool}</div>)}
+					<div className="tag role">{job.role}</div>
+					<div className="tag level">{job.level}</div>
+					{job.languages.map(lang => <div className="tag language">{lang}</div>)}
+					{job.tools.map(tool => <div className="tag language">{tool}</div>)}
 				</div>
 				
 			</div>
